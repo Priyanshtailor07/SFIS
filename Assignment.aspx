837f0c28588168ffc8b4603c35485219ff5e747b
Priyanshu Luharia <144381155+Priyanshtailor07@users.noreply.github.com>
2024-12-11 22:33:59 +0530

    FASSIGN

---
+++ Assignment.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/FacultyDashboard.Master" AutoEventWireup="true" CodeBehind="Assignment.aspx.cs" Inherits="StudentFacultyManagementSystem.Assignment" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style>
+        .form-control {
+            width: 300px;
+            margin-bottom: 20px;
+        }
+        .btn-submit {
+            margin-top: 20px;
+        }
+    </style>
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>Assignment Grading</h2>
+
+    <!-- Dropdown to select course -->
+    <div>
+        <label for="ddlCourses">Select Course:</label>
+        <asp:DropDownList ID="ddlCourses" runat="server" CssClass="form-control" AutoPostBack="True" OnSelectedIndexChanged="ddlCourses_SelectedIndexChanged">
+        </asp:DropDownList>
+    </div>
+
+    <!-- Dropdown to select question -->
+    <div>
+        <label for="ddlQuestions">Select Question:</label>
+        <asp:DropDownList ID="ddlQuestions" runat="server" CssClass="form-control" AutoPostBack="True" OnSelectedIndexChanged="ddlQuestions_SelectedIndexChanged">
+        </asp:DropDownList>
+    </div>
+
+    <!-- GridView to display student answers -->
+    <asp:GridView ID="gvStudentAnswers" runat="server" AutoGenerateColumns="False" CssClass="table" DataKeyNames="AnswerID" OnRowCommand="gvStudentAnswers_RowCommand">
+        <Columns>
+            <asp:BoundField DataField="StudentName" HeaderText="Student Name" />
+            <asp:BoundField DataField="AnswerText" HeaderText="Answer" />
+            <asp:BoundField DataField="Grade" HeaderText="Current Grade" />
+            <asp:TemplateField HeaderText="Update Grade">
+                <ItemTemplate>
+                    <asp:DropDownList ID="ddlGrades" runat="server">
+                        <asp:ListItem Text="A" Value="A"></asp:ListItem>
+                        <asp:ListItem Text="B" Value="B"></asp:ListItem>
+                        <asp:ListItem Text="C" Value="C"></asp:ListItem>
+                        <asp:ListItem Text="D" Value="D"></asp:ListItem>
+                        <asp:ListItem Text="F" Value="F"></asp:ListItem>
+                    </asp:DropDownList>
+                    <asp:Button CommandName="UpdateGrade" CommandArgument="<%# Container.DataItemIndex %>" Text="Save" runat="server" CssClass="btn btn-primary btn-submit" />
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+    </asp:GridView>
+</asp:Content>
---
+++ Assignment.aspx.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace StudentFacultyManagementSystem
+{
+    public partial class Assignment : System.Web.UI.Page
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["StudentFacultyDatabase"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindCourses();
+            }
+        }
+
+        private void BindCourses()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT CourseID, CourseName FROM Courses";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                ddlCourses.DataSource = dt;
+                ddlCourses.DataTextField = "CourseName";
+                ddlCourses.DataValueField = "CourseID";
+                ddlCourses.DataBind();
+            }
+
+            ddlCourses.Items.Insert(0, new ListItem("-- Select Course --", ""));
+        }
+
+        protected void ddlCourses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BindQuestions();
+        }
+
+        private void BindQuestions()
+        {
+            if (!string.IsNullOrEmpty(ddlCourses.SelectedValue))
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT QuestionID, QuestionText FROM Questions WHERE CourseID = @CourseID";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@CourseID", ddlCourses.SelectedValue);
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    ddlQuestions.DataSource = dt;
+                    ddlQuestions.DataTextField = "QuestionText";
+                    ddlQuestions.DataValueField = "QuestionID";
+                    ddlQuestions.DataBind();
+                }
+
+                ddlQuestions.Items.Insert(0, new ListItem("-- Select Question --", ""));
+            }
+        }
+
+        protected void ddlQuestions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BindStudentAnswers();
+        }
+
+        private void BindStudentAnswers()
+        {
+            if (!string.IsNullOrEmpty(ddlQuestions.SelectedValue))
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = @"
+                        SELECT 
+                            sa.AnswerID, 
+                            s.FirstName + ' ' + s.LastName AS StudentName, 
+                            sa.AnswerText, 
+                            sa.Grade 
+                        FROM StudentAnswers sa
+                        INNER JOIN Students s ON sa.StudentID = s.StudentID
+                        WHERE sa.QuestionID = @QuestionID";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@QuestionID", ddlQuestions.SelectedValue);
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    gvStudentAnswers.DataSource = dt;
+                    gvStudentAnswers.DataBind();
+                }
+            }
+        }
+
+        protected void gvStudentAnswers_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "UpdateGrade")
+            {
+                int rowIndex = Convert.ToInt32(e.CommandArgument);
+                GridViewRow row = gvStudentAnswers.Rows[rowIndex];
+                int answerID = Convert.ToInt32(gvStudentAnswers.DataKeys[rowIndex].Value);
+                DropDownList ddlGrades = (DropDownList)row.FindControl("ddlGrades");
+
+                if (ddlGrades != null)
+                {
+                    string selectedGrade = ddlGrades.SelectedValue;
+
+                    using (SqlConnection conn = new SqlConnection(connectionString))
+                    {
+                        string query = "UPDATE StudentAnswers SET Grade = @Grade WHERE AnswerID = @AnswerID";
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        cmd.Parameters.AddWithValue("@Grade", selectedGrade);
+                        cmd.Parameters.AddWithValue("@AnswerID", answerID);
+
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    BindStudentAnswers();
+                }
+            }
+        }
+    }
+}
